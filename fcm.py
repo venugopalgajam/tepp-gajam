e66c2cec043497b05a502d17ab2ba4efc8428712
Venu Gopal Gajam <Venu Gopal Gajam>
2018-03-31 10:19:11 +0530

    typo fcm.py

--- fcm.py
+++ fcm.py
@@ -3,4 +3,4 @@ import json
 def send_push(sub_str, data="registered!!"):
     print(sub_str)
     sub = json.loads(sub_str)
-    webpush(sub,data,vapid_private_key="kAk3nEapE9gVpidbjMkTFqivk1ic5V8kDn3N4dn_aEo",vapid_claims={"sub": "mailto:venugopalgajam@gmail.com"})
+    webpush(sub,data,vapid_private_key=json.load(open('./fcm-creds.json')),vapid_claims={"sub": "mailto:venugopalgajam@gmail.com"})
