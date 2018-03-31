f269e4e12e03211c6feb088ac2d311f6a96d608b
Venu Gopal Gajam <Venu Gopal Gajam>
2018-03-31 10:31:08 +0530

    typos corrected

--- fcm.py
+++ fcm.py
@@ -1,6 +1,5 @@
 from pywebpush import webpush
 import json
 def send_push(sub_str, data="registered!!"):
-    print(sub_str)
     sub = json.loads(sub_str)
     webpush(sub,data,vapid_private_key=json.load(open('./fcm-creds.json')),vapid_claims={"sub": "mailto:venugopalgajam@gmail.com"})
